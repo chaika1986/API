7da7fced34a895356bd361566a1a87d7088582e7
Chaika <chaika1986@mail.ru>
2022-08-11 23:10:30 +0700

    all files

---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+    public FormDate birthday = new FormDate() {
+
+    };
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+
+}
+
+
